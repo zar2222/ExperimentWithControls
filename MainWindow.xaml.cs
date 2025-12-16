5031ef94515621788e47ea5de02ffccd1c28712d
Better <misazarubin24@gmail.com>
2025-12-16 19:33:16 +0300

    version 0.0.1

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,5 +20,51 @@ namespace ExperimentWithControls
         {
             InitializeComponent();
         }
+
+		private void numberTextBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			number.Text = numberTextBox.Text;
+			
+		}
+
+		private void number_PreviewTextInput(object sender, TextCompositionEventArgs e)
+		{
+			e.Handled = !int.TryParse(e.Text, out int result);
+		}
+
+		private void smallSlide_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+		{
+			number.Text = smallSlider.Value.ToString("0");
+		}
+
+		private void RadioButton_Checked(object sender, RoutedEventArgs e)
+		{
+			RadioButton clickedRadio = (RadioButton)sender;
+			number.Text = clickedRadio.Content.ToString();
+		}
+
+		private void bigSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+		{
+			number.Text = bigSlider.Value.ToString("000-000-0000");
+		}
+
+		private void myListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			if (myListBox.SelectedItem is ListBoxItem listBoxItem)
+			{
+				number.Text = listBoxItem.Content.ToString();
+			}
+		}
+
+		private void readOnlyComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			if (readOnlyComboBox.SelectedItem is ListBoxItem listBoxItem) 
+			{number.Text = listBoxItem.Content.ToString();}
+		}
+
+		private void EditableComboBox_OnTextChanged(object sender, TextChangedEventArgs e)
+		{
+			if (sender is ComboBox comboBox) number.Text = comboBox.Text;
+		}
     }
 }
